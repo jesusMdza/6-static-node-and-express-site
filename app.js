b3ee46dc36d2e9943e6dbb3587ca8089853d0e8b
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-06-28 15:56:50 -0500

    Add to js and pug files
    Added basic routes to app.js
    Modified fields in about.pug
    Modified fields in layout.pug

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const projectData = require('./data.json');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    
+});
+
+app.get('/project', (req, res) => {
+    
+});
+
+app.listen(3000);
