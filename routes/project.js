9bb6e51c0a763b71008e288875c38265bd40e454
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-06-29 11:55:29 -0500

    Testing and rendering properties

--- routes/project.js
+++ routes/project.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const jsonData = require('../data.json');
 const projects = jsonData.projects;
 
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
     const id = req.params.id;
-    res.send(id);
+    const title = projects.project_name;
+    const description = projects.description;
+    const technologies = projects.technologies;
+    const liveLink = projects.live_link;
+    const githubLink = projects.github_link;
+    const image_urls = projects.image_urls;
+    
+    const templateData = { title , description };
+    
+    res.render('project', templateData);
 });
 
 module.exports = router;
