ab55af2aeb2ecc060fa3cfbec5b2ca1c24f58f2e
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-06-30 14:50:22 -0500

    Changes
    altered data.json
    corrected images
    added pic
    added additional variables project.js
    added elevator pitch, skills and links about.pug
    added info index.pug
    added info layout.pug
    added variables to template project.pug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const jsonData = require('../data.json');
+const projects = jsonData.projects;
 
 router.get('/', (req, res) => {
-    res.render('index');
+    const templateData = {projects};
+
+    res.render('index', templateData);
 });
 
 router.get('/about', (req, res) => {
--- routes/project.js
+++ routes/project.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const jsonData = require('../data.json');
 const projects = jsonData.projects;
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = req.params.id;
-    const title = projects.project_name;
-    const description = projects.description;
-    const technologies = projects.technologies;
-    const liveLink = projects.live_link;
-    const githubLink = projects.github_link;
-    const image_urls = projects.image_urls;
+    const title = projects[id].project_name;
+    const description = projects[id].description;
+    const technologies = projects[id].technologies;
+    const liveLink = projects[id].live_link;
+    const githubLink = projects[id].github_link;
+    const imageUrls = projects[id].image_urls;
     
-    const templateData = { title , description };
-    
+    const templateData = {projects, title, description, technologies, liveLink, githubLink, imageUrls};
+
     res.render('project', templateData);
 });
 
